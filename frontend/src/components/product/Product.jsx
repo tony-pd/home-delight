f1693ec68d9db587fbc67501d52af0ba226f4f15
Tony <mailtonypd@proton.me>
2024-11-13 08:41:39 +0530

    image url change

--- frontend/src/components/product/Product.jsx
+++ frontend/src/components/product/Product.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import './product.css';
 import AddToCart from "../addToCart/AddToCart";
+import { baseUrl } from '../../constants';
 
 const Product = ({ product }) => (
   <div className="product-item">
     <img className="product-image" 
-      src={product.image} 
+      src={baseUrl + product.image} 
       width={"100%"}
       height={"auto"}
       alt={product.title} />
