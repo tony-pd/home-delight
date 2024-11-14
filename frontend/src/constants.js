e8964e62562324dcb308d8aaebb675ea41d732aa
Tony <mailtonypd@proton.me>
2024-11-14 08:44:26 +0530

    change to render url

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,2 +1,3 @@
 //export const baseUrl = "http://localhost:3000";
-export const baseUrl = "https://home-delight-api.vercel.app";
+//export const baseUrl = "https://home-delight-api.vercel.app";
+export const baseUrl = "https://home-delight-backend.onrender.com";
